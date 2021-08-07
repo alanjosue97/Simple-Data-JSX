6448a7ab6f05b0474c0c33970764424707885721
alanjosue97 <perezaln.09@gmail.com>
2021-08-06 19:31:06 -0500

    I show simple data on display using JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import profile from "./assets/profile.jpg";
+
+//Our data
+const user = {
+  name: "Alan On Code",
+  location: "Las Vegas",
+  foodType: "Everything",
+  age: "23",
+  likes: "Coding into the wee hours of the morning ",
+  githubUsername: "alanjosue97",
+  picProfil: profile,
+};
+console.log(user);
 
 function App() {
+  const url = `https://github.com/${user.githubUsername}`;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="user-info">
+        <p>Simple Data JSX</p>
+        <img src={user.picProfil} className="img-profile" alt={user.name} />
+        <h3>
+          <a href={url}>{user.name}</a>
+        </h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>Location</strong>
+          {user.location}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <strong>Eats</strong>
+          {user.foodType}
+        </p>
+        <p>
+          <strong>Age</strong>
+          {user.age}
+        </p>
+        <p>
+          <strong>Likes</strong>
+          {user.likes}
+        </p>
+        <p>
+          <strong>GitHub</strong>
+          <a href={url}>@{user.githubUsername}</a>
+        </p>
+      </div>
     </div>
   );
 }
